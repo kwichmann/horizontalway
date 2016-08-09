4fdd803386a5800abc860c3e1dbc2fd4083b0405
kwichmann <kristian.wichmann@gmail.com>
2016-08-10 00:58:58 +0200

    Basic functionality added

--- sketch.js
+++ sketch.js
@@ -9,46 +9,60 @@ var stringsSel;
 var generateButton;
 var distanceRadio;
 
+var nameDiv;
+var h2;
+var canvasDiv;
+var canvas;
+var inputDiv;
+
 function preload() {
   fretboard = loadImage("images/fretboard_760.png");
 }
 
 function setup() {
+  
+  nameDiv = createDiv('');
   h2 = createElement('h2', '');
+  h2.parent(nameDiv);
   
-  createCanvas(760, 72);
+  canvasDiv = createDiv('');
+  canvas = createCanvas(760, 72);
+  canvas.parent(canvasDiv);
   
-  createP('');
-  
-  rootSel = createSlider(0, 11, 0, 1);
+  inputDiv = createDiv('');
+  rootSel = createSlider(0, 11, 3, 1);
+  rootSel.parent(inputDiv);
   rootSel.input(setName);
   
   familySel = createSelect();
+  familySel.parent(inputDiv);
   for (var i = 0; i < scales.length; i++) {
     familySel.option(scales[i].family, i);
   }
   familySel.changed(newModes);
 
   modeSel = createSelect();
+  modeSel.parent(inputDiv);
   setModes();
-  modeSel.changed(setName);
+  modeSel.input(setName);
 
   intervalSel = createSelect();
+  intervalSel.parent(inputDiv);
   for (var i = 0; i < intervals.length; i++) {
-    intervalSel.option(intervals[i], i+1);
+    intervalSel.option(intervals[i], i + 1);
   }
   
   stringsSel = createSelect();
+  stringsSel.parent(inputDiv);
   for (var i = 0; i < pairs.length; i++) {
     stringsSel.option(pairs[i][0] + ' and ' + pairs[i][1], i);
   }
 
-  createP('');
-  
   generateButton = createButton('Generate random');
   generateButton.mousePressed(generate);
   
   distanceRadio = createRadio();
+  distanceRadio.style('display', 'inline-block');
   distanceRadio.option('Adjacent strings', 4);
   distanceRadio.option('At most two strings apart', 8);
   distanceRadio.option('At most three strings apart', 11);
@@ -56,6 +70,10 @@ function setup() {
   distanceRadio.option('Any distance', 14);
   
   setName();
+
+  colorMode(HSB);
+  strokeWeight(2);
+  
 }
 
 function setName() {
@@ -83,7 +101,56 @@ function generate() {
 }
 
 function draw() {
-  background(255);
+  background(51);
   image(fretboard, 0, 0);
-  console.log(familySel.value());
+  
+  drawScale(pairs[stringsSel.value()][0], intervalSel.value());
+  drawScale(pairs[stringsSel.value()][1], 0);
+}
+
+function dot(string, fret, hu, root) {
+  if (string < 1 || string > 6 || fret < 0 || fret > 24) {
+    return null;
+  }
+  if (fret == 0) {
+    x = 13
+  } else {
+    x = 990 - 962 * pow (2, - (fret - .5) / 12);
+  }
+  y = string * 11.2 - 3;
+  if (root) {
+    stroke(0, 0, 255);
+  } else {
+    stroke(0);
+  }
+  fill(hu, 255, 255);
+  ellipse(x, y, 12, 8);
+  
+  noStroke();
+  fill(0, 0, 255);
+  ellipse(x - 2, y - 1, 3, 2);
+}
+
+function makeMode(scal, deg) {
+  var offset = scal[deg];
+  var shiftedNote;
+  var nMode = [];
+  for (var i = 0; i < 7; i++) {
+    shiftedNote = (scal[(i + deg) % 7] + 12 - offset) % 12;
+    nMode.push(shiftedNote);
+  }
+  return nMode;
+}
+
+function drawScale(string, cOffset) {
+  theMode = makeMode(scales[familySel.value()].notes, modeSel.value());
+  theMode.sort(function(a, b){return a-b});
+  
+  for (var i = 0; i < 7; i++) {
+    var fret = (rootSel.value() + theMode[i] - stringOffset[string]) % 12;
+    var hu = noteHues[(i + 7 - cOffset) % 7];
+    dot(string, fret, hu, i == 0);
+    dot(string, fret + 12, hu, i == 0);
+    dot(string, fret + 24, hu, i == 0);
+  }  
 }
